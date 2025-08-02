ffd64c8ee5d185fdba3406194d5504d1b39b78c6
Bihary Roland <rolandbihary@gmail.com>
2025-08-03 00:05:25 +0200

    home page descriptions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,25 @@ function App() {
 
   return (
     <>
-      <h1>Home</h1>
+
+      <h1 className="text-2xl font-bold mb-4">Mi az a Project Athena?</h1>
+      <p>A Project Athena egy tudástár / archívum szortírozó rendszer, ami előre megadott adathalmazból listáz ki érdekesebb és különlegesebb témákat, különböző témakörökben, amiket a felhasználók átnézhetnek.</p>
+      <p className="mt-2"><span className="font-bold">Fontos megjegyeznem</span>: Ezen infókat nem én írom, nem lektorálom, nem az én munkám, én csak összegyűjtöm őket a későbbiekre magamnak, és/vagy más látogatóknak.</p>
+
+      <h1 className="text-2xl font-bold my-4">Miért Project Athena?</h1>
+      <p>Könnyen megjegyezhető nevet szerettem volna adni a projektnek, és bár fenntartom a változtatás jogát, (annak ellenére, hogy nem tervezek élni vele) ezért hagytam a nevében a Project megnevezés, ami kódnév jellege mellett ad egy komolyabb hangzást neki.</p>
+      <p>Ami <span className="font-bold">Athena</span>-t illeti, a görög mitológiában ismert Pállász Áthéné után kapta a nevét, aki <a className="underline italic font-bold" href="https://hu.wikipedia.org/wiki/Pallasz_Athéné">a bölcsesség, az igazságos háború, a jog, az igazságosság, a művészetek, a kézművesség és a képzés istennője a görög mitológiában.</a></p>
+      <p className="italic">(Szóval minden, amit maga a projekt is képvisel.)</p>
+      <p> <span className="italic font-bold">Szent állata a bagoly volt</span>, ami mind a mai napig sok kontextusban a bölcsesség szimbolikus állata.</p>
+
+      <h1 className="text-2xl font-bold my-4">Miért indult el ez az egész?</h1>
+      <p className="font-bold">Az kezdeményezés nagyon egyszerű:</p>
+      <p>Olyan világot élünk, ahol mindenki kezd rákattani mind vállalati szinten, úgy fogyasztóként is a TikTok és hozzá hasonló klón projektek (Youtube Shorts, megújult Instagram felület, Facebook Reels, csak hogy párat említsek...) népbutító, ingerküszöb csökkentő, felhasználóját beszippantó rendszerére.</p>
+      <p>Kortól és nemtől függetlenül érezhető saját családtagjaink érdektelenedése a 2-3 percnél hosszab időt igénylő témák tekintetében, legyen az egy hosszabb olvasmány, vagy egy 15-20 perces ismeretterjesztő Youtube videó.</p>
+
+      <p className="my-4">Innen indult az alap ötlet, ugyanis bár töröltem a Tiktok-ot, a Youtube Shorts még mindíg kísért, és erre szeretnék egy mindentől mentes tudástárat létrehozni, hogy figyelem elterelés mentesen élvezhessük a hosszabb, érdekes témákat.</p>
+
+      <p>Ha magamon kívül másnak is segítek ezzel az "archívum rendezővel", akkor már megérte a kezdeményezés.</p>
     </>
   )
 }
