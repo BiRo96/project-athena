56b95cc450402c16e26b35a5353c70c58f6a2ab6
Bihary Roland <rolandbihary@gmail.com>
2025-08-02 20:50:08 +0200

    Routes with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import Splash from './components/splash'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
       <Splash/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='*' element={<h1>404</h1>} />
+      </Routes>
     </>
   )
 }
