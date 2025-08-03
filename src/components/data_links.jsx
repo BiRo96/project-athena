cc26fb98627c338b9a4c341037aebd4de59ea462
Bihary Roland <rolandbihary@gmail.com>
2025-08-03 21:55:03 +0200

    date formating, copy to clipboard button and only the title is the hyperlink

--- src/components/data_links.jsx
+++ src/components/data_links.jsx
@@ -27,43 +27,45 @@ function DataLinks({
     return (
         <div className="flex flex-col mt-4 border-t-2 border-b-2 p-4">
             <a href={url} target="_blank" rel="noopener noreferrer">
-
-                <h2 className="text-2xl font-bold">{title}</h2>
-                
-                <div className="flex flex-row gap-1.5 mb-2 p-2">
-                    <div className="flex flex-col justify-center"><FormatIcon format={format}/></div>
-                    <div className="flex flex-col justify-center">{source}</div>
-                </div>
-
-                <p>{tags.map(tag => {
-                    return <span className="mr-1 pb-1 pt-0 px-2 rounded-full bg-blue-600 dark:bg-blue-500 text-white dark:text-gray-200" key={tag}>{tag}</span>  
-                })}</p>
-                
-                <div className="flex flex-row gap-8 mt-4 mb-2">
-                    <div className="flex flex-col justify-center text-center">
-                        <CircleFlag countryCode={language} title={language} alt={language} className="w-6 h-6 my-3" />
-                    </div>
-                    <div className="flex flex-col justify-center text-center">
-                        <p>
-                            Bejegyzési idő: 
-                        </p>
-                        <p>
-                            {recordDate}
-                        </p>
-                    </div>
-                    {
-                        sourceDate !== "" &&
-                        <div className="flex flex-col justify-center text-center">
-                            <p>
-                                Forrás megjelenési ideje: 
-                            </p>
-                            <p>
-                                {sourceDate}
-                            </p>
-                        </div>
-                    }
-                </div>
+                <h2 className="text-2xl font-bold hover:underline">{title}</h2>
             </a>
+            
+            <div className="flex flex-row gap-1.5 mb-2 p-2">
+                <div className="flex flex-col justify-center"><FormatIcon format={format}/></div>
+                <div className="flex flex-col justify-center">{source}</div>
+            </div>
+
+            <p>{tags.map(tag => {
+                return <span className="mr-1 pb-1 pt-0 px-2 rounded-full bg-blue-600 dark:bg-blue-500 text-white dark:text-gray-200" key={tag}>{tag}</span>  
+            })}</p>
+            
+            <div className="flex flex-row gap-8 mt-4 mb-2">
+                <div className="flex flex-col justify-center text-center">
+                    <CircleFlag countryCode={language} title={language} alt={language} className="w-6 h-6 my-3" />
+                </div>
+                <div className="flex flex-col justify-center text-center">
+                    <p>
+                        Bejegyzési idő: 
+                    </p>
+                    <p>
+                        {recordDate !== "" && Intl.DateTimeFormat('hu-HU').format(new Date(recordDate.slice(0, 4) + "-" + recordDate.slice(5, 7) + "-" + recordDate.slice(8, 10)))}
+                    </p>
+                </div>
+                {
+                    sourceDate !== "" &&
+                    <div className="flex flex-col justify-center text-center">
+                        <p>
+                            Forrás megjelenési ideje: 
+                        </p>
+                        <p>
+                            {sourceDate !== "" && Intl.DateTimeFormat('hu-HU').format(new Date(sourceDate.slice(0, 4) + "-" + sourceDate.slice(5, 7) + "-" + sourceDate.slice(8, 10)))}
+                        </p>
+                    </div>
+                }
+                <div className="flex flex-col justify-center text-center" >
+                    <button className="text-blue-600 dark:text-blue-500 hover:underline" onClick={() => {navigator.clipboard.writeText(url)}}>Link másolása vágólapra</button>
+                </div>
+            </div>
         </div>
     )
 }
